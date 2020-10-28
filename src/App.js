4f0c3e69cec52b0901a8c471f8d95927c2c8e9af
Isaac Costa <ibandim@outlook.com>
2020-10-28 16:03:50 -0300

    add components react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React,{ Component } from 'react';
 //import logo from './logo.svg';
 //import './App.css';
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World</h1>
+import './style.css'
+import Header from  './components/Header'
+
+const App = () => (
+     <div className="App">
+        <Header />
       </div>
-      );
-    }
-}
+)
 
 export default App;
