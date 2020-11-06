2e738858a420191b8fdc6489e043dab8d6ba58fa
Isaac Costa <ibandim@outlook.com>
2020-11-06 08:27:28 -0300

    Product List

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import api from '../../service/api'
-
+import './style.css'
 
 export default class Main extends Component {
    
+    //Conceito de estado (armazenamento):
+    state = {
+        products: [],
+    }
     //Método executa uma ação logo que o componente é exibido em tela
     componentDidMount() {
         //função da mesma classe para exibir produtos
@@ -14,10 +18,26 @@ export default class Main extends Component {
         //executando Api utilizando o método Get
         const response = await api.get('/products');
 
-        console.log(response)
+        //Prencher o estado no array, atualizando os produtos
+        this.setState({ products: response.data.docs });
     }
 
     render() {
-        return <h1>Hello Rocketseat</h1>
+        //O trecho "this.state.products.length" ouviu queo estado products foi alterado.
+        //<h1>Contagem de produtos: {this.state.products.length}</h1>
+    const { product } = this.state;
+    
+    return  (
+        <div className="product-list">
+            { this.state.products.map(product => (
+                <article key={product._id}>
+                    <strong>{product.title}</strong>
+                    <p>{product.description}</p>
+
+                    <a href ="">Acessar</a>
+                </article>
+            ))}
+        </div>
+    )
     }
-}
+} 
