f72c7e0f19befc4588d1f90a6bf9f10fe6a391f5
Isaac Costa <ibandim@outlook.com>
2020-11-10 08:01:07 -0300

    Pages

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -7,25 +7,50 @@ export default class Main extends Component {
     //Conceito de estado (armazenamento):
     state = {
         products: [],
-    }
+        productInfo: {},
+        page: 1,
+    };
     //Método executa uma ação logo que o componente é exibido em tela
     componentDidMount() {
         //função da mesma classe para exibir produtos
-        this.loadProducts();
+        this.loadProducts(); 
     }
 
-    loadProducts = async () => {
+    loadProducts = async (page = 1) => {
         //executando Api utilizando o método Get
-        const response = await api.get('/products');
+        const response = await api.get(`/products?page=${page}`);
 
+        const { docs, ...productInfo} = response.data;
         //Prencher o estado no array, atualizando os produtos
-        this.setState({ products: response.data.docs });
-    }
+        this.setState({ products: docs, productInfo, page });
+    };
+
+    //Próxima página e página anterior
+
+    prevPage = () => {
+        const { page, productInfo } = this.state;
+
+        if ( page === 1 ) return; 
+
+        const pageNumber = page - 1; 
+
+        this.loadProducts(pageNumber);
+    };
+ 
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        if( page === productInfo.page ) return;
+
+        const pageNumber = page + 1;
+        
+        this.loadProducts(pageNumber);
+    };
 
     render() {
         //O trecho "this.state.products.length" ouviu queo estado products foi alterado.
         //<h1>Contagem de produtos: {this.state.products.length}</h1>
-    const { product } = this.state;
+    const { product,page,productInfo } = this.state;
     
     return  (
         <div className="product-list">
@@ -37,6 +62,10 @@ export default class Main extends Component {
                     <a href ="">Acessar</a>
                 </article>
             ))}
+            <div className="actions">
+                <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próximo</button>
+            </div>
         </div>
     )
     }
